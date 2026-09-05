e4e27aae56692a8569371eccaf914c133a50faef
Lorenzo <lorenzo@raphos.com>
2026-09-05 21:20:30 +0100

    update version

--- RaphosGeometry/Properties/AssemblyInfo.cs
+++ RaphosGeometry/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using Synera.Core.Implementation.ApplicationService;
 
-// Associates every Node in this assembly with the Raphos Geometry add-in (matches the
-// <Guid> in RaphosGeometry.addin). Without this the packer reports "No nodes were found".
 [assembly: AssemblySyneraAddin("b6608507-f9c2-431e-a291-2d37483168fc")]
-[assembly: AssemblySyneraDefaultLicense("com.raphos.geometry.module", "Raphos Geometry", "0.1.0.0")]
-
+#if RELEASE
+//[assembly: AssemblySyneraDefaultLicense("com.raphos.cable.bundling.module", "Raphos Cable Bundling", "1.1.0.0")]
+//[assembly: AssemblySyneraDefaultLicense("partner.c3282bea.tokens", "Raphos Cable Bundling", "1.1.0.0", 2000)]
+#endif
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("windows10.0.15063")]
